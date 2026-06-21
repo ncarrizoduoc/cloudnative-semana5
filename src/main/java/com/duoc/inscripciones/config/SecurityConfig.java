398a793aa8f628a38f750aaff6949721db0c4122
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-06-21 00:48:08 -0400

    Se agrega procesamiento de JWT para remover prefijo Bearer

--- src/main/java/com/duoc/inscripciones/config/SecurityConfig.java
+++ src/main/java/com/duoc/inscripciones/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +15,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-            .anyRequest().authenticated()).oauth2ResourceServer((oauth2) -> 
-                oauth2.jwt(Customizer.withDefaults()));
+            .anyRequest().authenticated()).oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults())
+                .bearerTokenResolver(customTokenResolver()));
         return http.build();
+    }
+
+    @Bean
+    public BearerTokenResolver customTokenResolver(){
+        return request -> {
+            String authHeader = request.getHeader("Authorization");
+
+            if(authHeader == null || authHeader.isBlank()){
+                return null;
+            }
+
+            if(authHeader.regionMatches(true, 0, "Bearer ", 0, 7)){
+                return authHeader.substring(7).trim();
+            }
+            return authHeader.trim();
+        };
     }
 
 }
