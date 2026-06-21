69572f3d65d6879bdcfecba8c1ddc3696a18c3d0
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-06-20 23:57:32 -0400

    Se modifican parametros de JWT

--- src/main/java/com/duoc/inscripciones/config/SecurityConfig.java
+++ src/main/java/com/duoc/inscripciones/config/SecurityConfig.java
@@ -15,8 +15,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize ->
-            authorize.anyRequest().authenticated()).oauth2ResourceServer((oauth2) -> 
+        http.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api/inscripciones").permitAll()
+            .anyRequest().authenticated()).oauth2ResourceServer((oauth2) -> 
                 oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
