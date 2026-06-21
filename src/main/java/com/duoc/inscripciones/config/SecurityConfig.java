f398ef468170c9ec7441dcae453e72394715490f
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-06-21 00:31:48 -0400

    Se modifica configuracion de autenticacion con JWT

--- src/main/java/com/duoc/inscripciones/config/SecurityConfig.java
+++ src/main/java/com/duoc/inscripciones/config/SecurityConfig.java
@@ -16,7 +16,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/api/inscripciones").permitAll()
             .anyRequest().authenticated()).oauth2ResourceServer((oauth2) -> 
                 oauth2.jwt(Customizer.withDefaults()));
         return http.build();
